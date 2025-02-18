c5f18cea40b7b03b11c4d416032d8e898448ce2f
Tommy Shelby <dhruv@remitso.com>
2025-02-18 19:04:02 +0530

    Axios Globally Added and Request Interceptor Added

--- src/main.js
+++ src/main.js
@@ -5,10 +5,23 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL
+axios.interceptors.request.use((config) => {
+    config.headers['Accept'] = 'application/json'
+    config.headers['X-App-User'] = import.meta.env.VITE_APP_USERNAME
+    config.headers['X-App-Password'] = import.meta.env.VITE_APP_PASSWORD
+
+    return config;
+})
+app.provide('axios', axios)
+
 app.mount('#app')
+
+
