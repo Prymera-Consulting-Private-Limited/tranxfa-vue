5be81c412302af582cbca0a5554d298275f13631
Tommy Shelby <dhruv@remitso.com>
2025-02-19 16:35:03 +0530

    Models Added

---
+++ src/models/account.js
@@ -0,0 +1,24 @@
+class Account {
+    /**
+     * @type {string|null}
+     */
+    email = null;
+    /**
+     * @type {boolean|null}
+     */
+    isEmailVerified = null;
+    /**
+     * @type {string|null}
+     */
+    passwordChangedAt = null;
+    /**
+     * @type {string|null}
+     */
+    createdAt = null;
+    /**
+     * @type {string|null}
+     */
+    updatedAt = null;
+}
+
+export default Account;
