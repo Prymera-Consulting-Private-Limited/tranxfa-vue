672a0ff3bc1121781bce340c89a9b643bda2b3ff
Tommy Shelby <dhruv@remitso.com>
2025-02-20 19:38:21 +0530

    Login Navigation Machine Added

---
+++ src/machines/login_navigation_machine.js
@@ -0,0 +1,39 @@
+import {assign, createMachine} from 'xstate';
+export const loginNavigationMachine = createMachine({
+    id: 'loginNavigation',
+    initial: 'login',
+    context: {
+        isEmailVerified: false,
+        identityInfoProvided: false,
+    },
+    states: {
+        login: {
+            on: {
+                PROCEED: [
+                    { target: "emailVerification", guard: "emailVerificationRequired" },
+                    { target: "updateIdentityInformation", guard: "identityInformationRequired" },
+                    { target: "dashboard" },
+                ],
+                SET_CONTEXT: {
+                    actions: assign({
+                        isEmailVerified: ({context, event}) => {
+                            return event.isEmailVerified || context.isEmailVerified;
+                        },
+                        identityInfoProvided: ({context, event}) => {
+                            console.log(event.identityInfoProvided || context.identityInfoProvided);
+                            return event.identityInfoProvided || context.identityInfoProvided;
+                        }
+                    })
+                }
+            },
+        },
+        emailVerification: {},
+        updateIdentityInformation: {},
+        dashboard: {},
+    }
+}, {
+    guards: {
+        emailVerificationRequired: ({context}) => ! context.isEmailVerified,
+        identityInformationRequired: ({context}) => ! context.identityInfoProvided
+    }
+});
