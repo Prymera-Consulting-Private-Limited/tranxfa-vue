21a2d375936174861661cb727c66b8e584fc45e5
Tommy Shelby <dhruv@remitso.com>
2025-02-21 00:42:58 +0530

    View Updated

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,8 +1,34 @@
-<template>
-  <CustomerLayout>
-
-  </CustomerLayout>
-</template>
 <script setup>
 import CustomerLayout from "@/components/CustomerLayout.vue";
+import {useCustomerStore} from "@/stores/customer.js";
+import {useCustomerUtils} from "@/composables/customer_utils.js";
+import {onMounted} from "vue";
+
+const customerStore = useCustomerStore();
+const customerUtils = useCustomerUtils();
+
+onMounted(async () => {
+  if (! customerStore.isLoaded) {
+    await customerUtils.refresh();
+  }
+});
 </script>
+<template>
+  <CustomerLayout>
+    <section class="flex w-full tracking-wider">
+      <div class="container mx-auto ">
+        <div class="">
+          <div class="flex-1 p-4 md:p-6">
+            <!-- Welcome Section -->
+            <div class="bg-white p-4 md:p-6 shadow-sm rounded-lg">
+              <h1 class="text-2xl font-semibold text-purple-700 mb-2 flex items-center">
+                Welcome {{ customerStore.customer.fullName }}
+              </h1>
+              <p class="text-sm text-gray-500">Your account overview</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </CustomerLayout>
+</template>
