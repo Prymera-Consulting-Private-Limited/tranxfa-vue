3484053922abe096b99351d14e431e07f2984564
Tommy Shelby <dhruv@remitso.com>
2025-03-06 23:15:59 +0530

    Add dynamic id and name attributes to input for better form handling

--- src/components/CustomerAttribute/TextInput.vue
+++ src/components/CustomerAttribute/TextInput.vue
@@ -7,5 +7,5 @@ defineProps({
 </script>
 
 <template>
-  <input type="text" v-model="attr.value" class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
+  <input :id="attr.attribute" :name="attr.attribute" type="text" v-model="attr.value" class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
 </template>
