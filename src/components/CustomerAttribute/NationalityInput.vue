d916339fcc09a0b8c4633e346f05bdf25bcbd76b
Tommy Shelby <dhruv@remitso.com>
2025-03-06 23:48:32 +0530

    Refactor nationality input to use CountryVSelectInput component

--- src/components/CustomerAttribute/NationalityInput.vue
+++ src/components/CustomerAttribute/NationalityInput.vue
@@ -1,9 +1,8 @@
 <script setup>
 import CustomerAttribute from "@/models/customer_attribute.js";
-import vSelect from 'vue-select';
 import {useCountryUtils} from "@/composables/country_utils.js";
-import {computed, h, onMounted, ref} from "vue";
-import FlagIcon from 'vue3-flag-icons'
+import {computed, onMounted, ref} from "vue";
+import CountryVSelectInput from "@/components/CustomerAttribute/CountryVSelectInput.vue";
 
 const props = defineProps({
   attr: CustomerAttribute,
@@ -24,43 +23,17 @@ const nationality = computed({
   }
 })
 
-vSelect.props.components.default = () => ({
-  Deselect: {
-    render: () => h('span', h('i', {class: ['pi', 'pi-times', 'text-gray-500', 'text-xs']})),
-  },
-  OpenIndicator: {
-    render: () => h('span', h('i', {class: ['pi', 'pi-chevron-down', 'text-gray-500', 'text-xs']})),
-  },
-});
-
 onMounted(async () => {
   countries.value = await countryUtils.getCountries();
 })
 
+function updateNationality(updated) {
+  nationality.value = updated;
+}
 </script>
 
 <template>
-  <v-select v-model="nationality" :options="countries" :placeholder="`Please select`" key-by="id" label="demonym">
-    <template v-slot:no-options="{ search, searching }">
-      <template class="text-sm text-gray-300" v-if="searching">No results found for <em>{{ search }}</em>.</template>
-      <em class="text-sm text-gray-400" v-else style="opacity: 0.5">Start typing to search for a country.</em>
-    </template>
-    <template #selected-option-container="{ option, deselect, multiple, disabled }">
-      <div class="vs__selected">
-        <div class="flex items-center w-auto">
-          <div class="text-sm flex items-center w-full gap-x-2">
-            <FlagIcon :code="option.iso2Alpha.toLowerCase()" circle  />
-            <span class="lg:max-w-sm xl:max-w-md truncate">{{ option.demonym }}</span>
-          </div>
-        </div>
-      </div>
-    </template>
+  <CountryVSelectInput v-bind:countries="countries" v-bind:model="nationality" v-bind:labelGenerator="`demonym`" v-on:update:modelValue="updateNationality" :attr="attr">
 
-    <template #option="option">
-      <div class="text-sm flex items-center w-full gap-x-3 truncate">
-        <FlagIcon :code="option.iso2Alpha.toLowerCase()" circle  />
-        <span class="truncate">{{ option.demonym }}</span>
-      </div>
-    </template>
-  </v-select>
+  </CountryVSelectInput>
 </template>
