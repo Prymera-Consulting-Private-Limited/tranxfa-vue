4cc63f6fcb788c51194d0afd37722cf9017c821c
Tommy Shelby <dhruv@remitso.com>
2025-03-08 01:25:31 +0530

    Add static method to Customer class for instance creation from API data

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,3 +1,9 @@
+import Account from "@/models/account.js";
+import Session from "@/models/session.js";
+import Country from "@/models/country.js";
+import CustomerAddress from "@/models/customer_address.js";
+import CustomerAttribute from "@/models/customer_attribute.js";
+
 export class Customer {
     /**
      * @type {string|null}
@@ -91,6 +97,47 @@ export class Customer {
 
     identityInformationRequired () {
         return Boolean(this.attributes.find((o) => o.category === 'identity' && o.isRequired && !o.value))
+    }
+
+    static getInstance(data) {
+        const customer = new Customer();
+        customer.id = data.id;
+        customer.crn = data.crn;
+        customer.firstName = data.first_name;
+        customer.middleName = data.middle_name;
+        customer.lastName = data.last_name;
+        customer.fullName = data.full_name;
+        customer.uniqueIdentityNumber = data.unique_identity_number;
+        customer.mobileNumber = data.mobile_number;
+        customer.createdAt = data.created_at;
+        customer.updatedAt = data.updated_at;
+        if (data.account) {
+            customer.account = Account.getInstance(data.account);
+        }
+        if (data.session) {
+            customer.session = Session.getInstance(data.session);
+        }
+        if (data.mobile_number_country) {
+            customer.mobileNumberCountry = Country.getInstance(data.mobile_number_country);
+        }
+        if (data.country) {
+            customer.country = Country.getInstance(data.country);
+        }
+        if (data.nationality) {
+            customer.nationality = Country.getInstance(data.nationality);
+        }
+        if (data.address) {
+            customer.address = CustomerAddress.getInstance(data.address);
+        }
+        if (data.attributes && data.attributes.length > 0) {
+            let attributes = [];
+            for (const attribute of data.attributes) {
+                attributes.push(CustomerAttribute.getInstance(attribute));
+            }
+
+            customer.attributes = attributes;
+        }
+        return customer;
     }
 }
 
