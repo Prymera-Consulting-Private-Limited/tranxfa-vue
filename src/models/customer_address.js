0f2a8edb7e0fa1b9178ab74cb893b2ef5fa2eedd
Tommy Shelby <dhruv@remitso.com>
2025-03-08 01:24:57 +0530

    Add static method to CustomerAddress class for instance creation from API data

--- src/models/customer_address.js
+++ src/models/customer_address.js
@@ -33,6 +33,18 @@ class CustomerAddress {
      * @type {string|null}
      */
     updatedAt = null;
+
+    static getInstance(data) {
+        const address = new CustomerAddress();
+        address.addressLine1 = data.address_line_1;
+        address.addressLine2 = data.address_line_2;
+        address.city = data.city;
+        address.postcode = data.postcode;
+        address.region = data.region;
+        address.createdAt = data.created_at;
+        address.updatedAt = data.updated_at;
+        return address;
+    }
 }
 
 export default CustomerAddress;
