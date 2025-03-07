3597c7ce210c75a893a3b8eb6292239381addefa
Tommy Shelby <dhruv@remitso.com>
2025-03-08 01:08:26 +0530

    Add static method to Currency class for instance creation from API data

--- src/models/currency.js
+++ src/models/currency.js
@@ -48,6 +48,21 @@ class Currency {
      * @type {string|null}
      */
     iconUnicode = null;
+
+    static getInstance(data) {
+        const currency = new Currency();
+        currency.id = data.id;
+        currency.decimalPlaces = data.decimal_places;
+        currency.isoNumeric = data.iso_numeric;
+        currency.isoAlpha = data.iso_alpha;
+        currency.currencyType = data.type;
+        currency.commonName = data.common_name;
+        currency.officialName = data.official_name;
+        currency.cryptoCode = data.crypto_code;
+        currency.code = data.code;
+        currency.iconUnicode = data.icon_unicode;
+        return currency;
+    }
 }
 
 export default Currency;
