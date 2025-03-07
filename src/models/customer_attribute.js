d03e2646f28c52671d0568a03765bd3b7b68ce4e
Tommy Shelby <dhruv@remitso.com>
2025-03-08 01:25:16 +0530

    Add static method to CustomerAttribute class for instance creation from API data

--- src/models/customer_attribute.js
+++ src/models/customer_attribute.js
@@ -43,6 +43,20 @@ class CustomerAttribute {
      * @type {string|null}
      */
     value = null;
+
+    static getInstance(data) {
+        const attribute = new CustomerAttribute();
+        attribute.attribute = data.attribute;
+        attribute.category = data.category;
+        attribute.regularExpression = data.regular_expression;
+        attribute.mask = data.mask;
+        attribute.expressionErrorMessage = data.expression_error_message;
+        attribute.label = data.label;
+        attribute.infoText = data.info_text;
+        attribute.isRequired = data.is_required;
+        attribute.value = data.value;
+        return attribute;
+    }
 }
 
 export default CustomerAttribute;
