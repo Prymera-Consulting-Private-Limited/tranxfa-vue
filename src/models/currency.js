f9c5cdce71a5e744a7b630ce64d2e9220620c3ea
Tommy Shelby <dhruv@remitso.com>
2025-03-08 00:54:16 +0530

    Add currency model

---
+++ src/models/currency.js
@@ -0,0 +1,53 @@
+class Currency {
+    /**
+     * @type {string|null}
+     */
+    id = null;
+
+    /**
+     * @type {int|null}
+     */
+    decimalPlaces = null;
+
+    /**
+     * @type {string|null}
+     */
+    isoNumeric = null;
+
+    /**
+     * @type {string|null}
+     */
+    isoAlpha = null;
+
+    /**
+     * @type {string|null}
+     */
+    currencyType = null;
+
+    /**
+     * @type {string|null}
+     */
+    commonName = null;
+
+    /**
+     * @type {string|null}
+     */
+    officialName = null;
+
+    /**
+     * @type {string|null}
+     */
+    cryptoCode = null;
+
+    /**
+     * @type {string|null}
+     */
+    code = null;
+
+    /**
+     * @type {string|null}
+     */
+    iconUnicode = null;
+}
+
+export default Currency;
