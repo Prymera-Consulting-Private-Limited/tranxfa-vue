13d741d3ed72e284b4eda2019c99f193b8e8ca8f
Tommy Shelby <dhruv@remitso.com>
2025-03-08 07:36:10 +0530

    Component Added

---
+++ src/components/MoneyInputShimmer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center rounded-md outline-2 outline-gray-300 bg-white">
+    <div class="h-12 pulse rounded-md bg-gray-300 w-full"></div>
+  </div>
+</template>
+<style scoped>
+@keyframes heartbeat-opacity {
+  0%, 100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+}
+
+.pulse {
+  animation: heartbeat-opacity 1.5s infinite ease-in-out;
+}
+</style>
+<script setup>
+</script>
