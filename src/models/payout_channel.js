5da7b45cd0e95ac3735fca5ebaafb952e4738b72
Tommy Shelby <dhruv@remitso.com>
2025-03-08 17:18:18 +0530

    Add payout channel model

---
+++ src/models/payout_channel.js
@@ -0,0 +1,35 @@
+import PayoutChannelConfiguration from "@/models/payout_channel_configuration.js";
+import PayoutChannelAttribute from "@/models/payout_channel_attributes.js";
+
+class PayoutChannel {
+    /**
+     * * @type {string|null}
+     */
+    id = null;
+
+    /**
+     * @type {PayoutChannelConfiguration|null}
+     */
+    configuration = null;
+
+    /**
+     * @type {PayoutChannelAttribute[]}
+     */
+    attributes = [];
+
+    static getInstance(data) {
+        const payoutChannel = new PayoutChannel();
+        payoutChannel.id = data.id;
+        if (data.configuration) {
+            payoutChannel.configuration = PayoutChannelConfiguration.getInstance(data.configuration);
+        }
+        if (data.attributes) {
+            payoutChannel.attributes = data.attributes.map((attribute) => {
+                return PayoutChannelAttribute.getInstance(attribute);
+            });
+        }
+        return payoutChannel;
+    }
+}
+
+export default PayoutChannel;
