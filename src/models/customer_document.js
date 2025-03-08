e8ad17a6a01c4bd4f90059f1cd6cb6356f7cde1a
Tommy Shelby <dhruv@remitso.com>
2025-03-08 20:36:23 +0530

    Add KycDocumentStatus enum and DocumentCategory model to CustomerDocument

--- src/models/customer_document.js
+++ src/models/customer_document.js
@@ -1,3 +1,6 @@
+import KycDocumentStatus from "@/enums/kyc_document_status.js";
+import DocumentCategory from "@/models/document_category.js";
+
 class CustomerDocument {
     /**
      * @type {String|null}
@@ -5,17 +8,17 @@ class CustomerDocument {
     id = null;
 
     /**
-     * @type {String|null}
+     * @type {DocumentCategory|null}
      */
     documentCategory = null;
 
     /**
-     * @type {String|null}
+     * @type {DocumentType|null}
      */
     documentType = null;
 
     /**
-     * @type {String|null}
+     * @type {KycDocumentStatus|null}
      */
     statusCode = null;
 
@@ -33,6 +36,22 @@ class CustomerDocument {
      * @type {String|null}
      */
     updatedAt = null;
+
+    static getInstance(data) {
+        const document = new CustomerDocument();
+        document.id = data.id;
+        if (data.document_category) {
+            document.documentCategory = DocumentCategory.getInstance(data.document_category);
+        }
+        if (data.document_type) {
+            document.documentType = DocumentType.getInstance(data.document_type);
+        }
+        document.statusCode = KycDocumentStatus[data.status_code];
+        document.statusTitle = data.status_title;
+        document.createdAt = data.created_at;
+        document.updatedAt = data.updated_at;
+        return document;
+    }
 }
 
 export default CustomerDocument;
