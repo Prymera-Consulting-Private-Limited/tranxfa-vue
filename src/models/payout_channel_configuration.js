4827d6d3c231931ca5ba9b423fab81b8b60a2d69
Tommy Shelby <dhruv@remitso.com>
2025-03-08 17:04:35 +0530

    Model Added

---
+++ src/models/payout_channel_configuration.js
@@ -0,0 +1,22 @@
+import RecipientType from "@/enums/recipient_type.js";
+
+class PayoutChannelConfiguration {
+    /**
+     * @type {RecipientType|null}
+     */
+    recipientType = null;
+
+    /**
+     * @type {boolean}
+     */
+    askForMiddleName = false;
+
+    static getInstance(data) {
+        const payoutChannelConfiguration = new PayoutChannelConfiguration();
+        payoutChannelConfiguration.recipientType = data.recipient_type;
+        payoutChannelConfiguration.askForMiddleName = data.ask_for_middle_name;
+        return payoutChannelConfiguration;
+    }
+}
+
+export default PayoutChannelConfiguration;
