d8af94fdc0a5dedeb4c3dd568349b366f9e20553
Tommy Shelby <dhruv@remitso.com>
2025-03-08 17:17:35 +0530

    Enum Added

---
+++ src/enums/recipient_data_type.js
@@ -0,0 +1,19 @@
+class RecipientDataType {
+    static TEXT = 'text';
+    static SELECT = 'select';
+    static RADIO = 'radio';
+    static DELIVERY_OPTION = 'delivery_option';
+    static ACCOUNT_NUMBER = 'account_number';
+    static PHONE_NUMBER = 'phone_number';
+    static MOBILE_NUMBER = 'mobile_number';
+    static EMAIL = 'email';
+    static ADDRESS_LINE_1 = 'address_line_1';
+    static ADDRESS_LINE_2 = 'address_line_2';
+    static ADDRESS_LINE_3 = 'address_line_3';
+    static ADDRESS_CITY = 'address_city';
+    static ADDRESS_REGION = 'address_region';
+    static ADDRESS_POSTCODE = 'address_postcode';
+    static ACCOUNT_HOLDER_NAME = 'account_holder_name';
+}
+
+export default RecipientDataType;
