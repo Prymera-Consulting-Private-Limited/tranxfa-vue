048ecc4250aa60a87615d635b35486d8fb664daf
Tommy Shelby <dhruv@remitso.com>
2025-03-08 21:45:33 +0530

    Add new properties to DocumentType for enhanced data handling

--- src/models/document_type.js
+++ src/models/document_type.js
@@ -19,12 +19,36 @@ class DocumentType {
      */
     description = null;
 
+    /**
+     * @type {Boolean}
+     */
+    publicUpload = false;
+
+    /**
+     * @type {String|null}
+     */
+    info = null;
+
+    /**
+     * @type {String|null}
+     */
+    documentNumberLabel = null;
+
+    /**
+     * @type {String|null}
+     */
+    api = null;
+
     static getInstance(data) {
         const type = new DocumentType();
         type.id = data.id;
         type.code = data.code;
         type.title = data.title;
         type.description = data.description;
+        type.publicUpload = data?.public_upload ?? false;
+        type.info = data?.info;
+        type.documentNumberLabel = data?.document_number_label;
+        type.api = data?.api;
         return type;
     }
 }
