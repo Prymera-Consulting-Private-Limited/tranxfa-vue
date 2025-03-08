c2e852419136604e876e0689b3c7960261ad0126
Tommy Shelby <dhruv@remitso.com>
2025-03-08 21:45:43 +0530

    Add documentTypes property to DocumentCategory for improved data structure

--- src/models/document_category.js
+++ src/models/document_category.js
@@ -19,12 +19,21 @@ class DocumentCategory {
      */
     description = null;
 
+    /**
+     * @type {DocumentType[]}
+     * @description List of document types that belong to this category
+     */
+    documentTypes = [];
+
     static getInstance(data) {
         const category = new DocumentCategory();
         category.id = data.id;
         category.code = data.code;
         category.title = data.title;
         category.description = data.description;
+        if (data.document_types) {
+            category.documentTypes = data.document_types.map((data) => DocumentType.getInstance(data))
+        }
         return category;
     }
 }
