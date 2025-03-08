43e8e3065a2de9df4d31ce9c680d1b751a8df515
Tommy Shelby <dhruv@remitso.com>
2025-03-08 20:31:20 +0530

    Model Added

---
+++ src/models/customer_document.js
@@ -0,0 +1,38 @@
+class CustomerDocument {
+    /**
+     * @type {String|null}
+     */
+    id = null;
+
+    /**
+     * @type {String|null}
+     */
+    documentCategory = null;
+
+    /**
+     * @type {String|null}
+     */
+    documentType = null;
+
+    /**
+     * @type {String|null}
+     */
+    statusCode = null;
+
+    /**
+     * @type {String|null}
+     */
+    statusTitle = null;
+
+    /**
+     * @type {String|null}
+     */
+    createdAt = null
+
+    /**
+     * @type {String|null}
+     */
+    updatedAt = null;
+}
+
+export default CustomerDocument;
