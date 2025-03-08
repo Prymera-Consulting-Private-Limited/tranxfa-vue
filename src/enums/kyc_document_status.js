1f7821965cbe0671eec07b4b6e459c7cc5c2b54b
Tommy Shelby <dhruv@remitso.com>
2025-03-08 20:33:15 +0530

    Enum Added

---
+++ src/enums/kyc_document_status.js
@@ -0,0 +1,9 @@
+class KycDocumentStatus {
+    static PENDING_VERIFICATION = 'pending-verification';
+    static PROCESSING = 'processing';
+    static REVIEW_REQUIRED = 'review-required';
+    static APPROVED = 'approved';
+    static REJECTED = 'rejected';
+}
+
+export default KycDocumentStatus;
