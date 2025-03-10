01821c32a3ddaea860c45bdf0886309adf375771
Tommy Shelby <dhruv@remitso.com>
2025-03-10 21:48:33 +0530

    Add laravel-echo and pusher-js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios";
 
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
 const app = createApp(App)
 
 app.use(createPinia())
@@ -32,5 +35,16 @@ axios.interceptors.response.use(null, function (e) {
 app.provide('axios', axios)
 
 app.mount('#app')
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+});
 
 
