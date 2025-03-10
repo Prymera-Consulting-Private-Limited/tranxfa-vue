7e7272aea9ecfccf536cba2520b65792ee5f8048
Tommy Shelby <dhruv@remitso.com>
2025-03-10 21:49:18 +0530

    Add real-time event listeners for customer document processing

--- src/components/CustomerLayout.vue
+++ src/components/CustomerLayout.vue
@@ -1,6 +1,31 @@
 <script setup>
 import Footer from "@/components/Footer.vue";
 import Header from "@/components/Header.vue";
+import {useCustomerStore} from "@/stores/customer.js";
+import {onMounted} from "vue";
+import {useCustomerUtils} from "@/composables/customer_utils.js";
+
+const customerStore = useCustomerStore();
+const customerUtils = useCustomerUtils();
+
+onMounted(async () => {
+  if (customerStore.isLoaded === false) {
+    await customerUtils.refresh();
+  }
+  Echo.channel(`customer.${customerStore.customer.id}`)
+      .listen('CustomerDocumentProcessing', (e) => {
+        console.log(e);
+      })
+      .listen('CustomerDocumentReviewRequired', (e) => {
+        console.log(e);
+      })
+      .listen('CustomerDocumentApproved', (e) => {
+        console.log(e);
+      })
+      .listen('CustomerDocumentRejected', (e) => {
+        console.log(e);
+      });
+})
 </script>
 
 <template>
