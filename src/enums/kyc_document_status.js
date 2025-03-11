f3767270212f4a3ca59eed9196325de2f2660e72
Tommy Shelby <dhruv@remitso.com>
2025-03-11 17:21:31 +0530

    Refactor KycDocumentStatus to use Object.freeze for immutability and update statusCode assignment in customer_document.js

--- src/enums/kyc_document_status.js
+++ src/enums/kyc_document_status.js
@@ -1,9 +1,9 @@
-class KycDocumentStatus {
-    static PENDING_VERIFICATION = 'pending-verification';
-    static PROCESSING = 'processing';
-    static REVIEW_REQUIRED = 'review-required';
-    static APPROVED = 'approved';
-    static REJECTED = 'rejected';
-}
+const KycDocumentStatus = Object.freeze({
+    PENDING_VERIFICATION: 'pending-verification',
+    PROCESSING: 'processing',
+    REVIEW_REQUIRED: 'review-required',
+    APPROVED: 'approved',
+    REJECTED: 'rejected',
+})
 
-export default KycDocumentStatus;
+export default KycDocumentStatus
--- src/models/customer_document.js
+++ src/models/customer_document.js
@@ -1,4 +1,3 @@
-import KycDocumentStatus from "@/enums/kyc_document_status.js";
 import DocumentCategory from "@/models/document_category.js";
 import DocumentType from "@/models/document_type.js";
 
@@ -19,7 +18,7 @@ class CustomerDocument {
     documentType = null;
 
     /**
-     * @type {KycDocumentStatus|null}
+     * @type {String|null}
      */
     statusCode = null;
 
@@ -47,7 +46,7 @@ class CustomerDocument {
         if (data.document_type) {
             document.documentType = DocumentType.getInstance(data.document_type);
         }
-        document.statusCode = KycDocumentStatus[data.status_code];
+        document.statusCode = data.status_code;
         document.statusTitle = data.status_title;
         document.createdAt = data.created_at;
         document.updatedAt = data.updated_at;
