c5d4179d9d318e345fec0f06ba08271fdd97dcbb
Tommy Shelby <dhruv@remitso.com>
2025-03-14 20:49:07 +0530

    feat: Rename disableTargetSelection prop to disableSelection in Calculator and MoneyInput components

--- src/components/MoneyInput.vue
+++ src/components/MoneyInput.vue
@@ -21,7 +21,7 @@ const props = defineProps({
     currency: Currency
   })),
   inputId: String,
-  disableTargetSelection: {
+  disableSelection: {
     type: Boolean,
     default: false
   }
@@ -70,11 +70,11 @@ function amountUpdated() {
           <MenuButton as="div" :class="{'cursor-pointer': options.length > 1 && ! disableTargetSelection}" class="inline-flex w-full items-center justify-center rounded-r-md bg-brand-700 px-4 py-4 text-sm font-medium text-white">
             <FlagIcon :class="['ring-2 ring-white']" :code="country.iso2Alpha.toLowerCase()" circle />
             <strong class="text-sm tracking-wider ml-2">{{ currency.code }}</strong>
-            <ChevronDownIcon v-if="options.length > 1 && !disableTargetSelection" class="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100" aria-hidden="true"/>
+            <ChevronDownIcon v-if="options.length > 1 && !disableSelection" class="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100" aria-hidden="true"/>
           </MenuButton>
         </div>
         <transition enter-active-class="transition duration-100 ease-out" enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
-          <MenuItems v-if="options.length > 1 && !disableTargetSelection" class="absolute right-0 mt-2 w-86 origin-top-right divide-y divide-gray-300 rounded-md bg-gray-50 shadow-lg ring-1 ring-black/5 focus:outline-none z-99">
+          <MenuItems v-if="options.length > 1 && !disableSelection" class="absolute right-0 mt-2 w-86 origin-top-right divide-y divide-gray-300 rounded-md bg-gray-50 shadow-lg ring-1 ring-black/5 focus:outline-none z-99">
             <MenuItem v-for="(option, index) in options" v-slot="{ active }">
               <button @click="selectOption(option)" :class="[
                 active ? 'text-purple-900' : 'text-gray-900',
