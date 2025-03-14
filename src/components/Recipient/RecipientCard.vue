8a869a7d882dd3cb31b6c1ac276085520bef708a
Tommy Shelby <dhruv@remitso.com>
2025-03-14 22:24:44 +0530

    feat: Add conditional rendering for country and payout method in RecipientCard

--- src/components/Recipient/RecipientCard.vue
+++ src/components/Recipient/RecipientCard.vue
@@ -21,15 +21,20 @@ defineProps({
     </div>
     <h3 class="mt-6 text-sm font-medium text-gray-900">{{ recipient.fullName }}</h3>
     <dl class="mt-1 flex grow flex-col justify-between">
-      <dt class="sr-only">In Country</dt>
-      <dd class="text-gray-500 text-sm leading-5 flex items-center mx-auto gap-x-2">
-        <FlagIcon :code="recipient.channel.country.iso2Alpha.toLowerCase()" circle  />
-        {{ recipient.channel.country.commonName }}
-      </dd>
-      <dt class="sr-only">Payout Method</dt>
-      <dd class="mt-3">
-        <span class="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">{{ recipient.channel.payoutMethod.title }}</span>
-      </dd>
+      <template v-if="recipient?.channel?.country">
+        <dt class="sr-only">In Country</dt>
+        <dd class="text-gray-500 text-sm leading-5 flex items-center mx-auto gap-x-2">
+          <FlagIcon :code="recipient.channel.country.iso2Alpha.toLowerCase()" circle  />
+          {{ recipient.channel.country.commonName }}
+        </dd>
+      </template>
+      <template v-if="recipient?.channel?.payoutMethod">
+        <dt class="sr-only">Payout Method</dt>
+        <dd class="mt-3">
+          <span class="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">{{ recipient.channel.payoutMethod.title }}</span>
+        </dd>
+      </template>
+      <slot />
     </dl>
   </div>
 </template>
