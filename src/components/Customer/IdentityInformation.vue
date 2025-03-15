66ba0b4d5b2cf38b84f6bc2e898e1b26edd7ce00
Tommy Shelby <dhruv@remitso.com>
2025-03-15 17:25:05 +0530

    Added component

---
+++ src/components/Customer/IdentityInformation.vue
@@ -0,0 +1,73 @@
+<script setup>
+import {useCustomerStore} from "@/stores/customer.js";
+import {computed, onMounted, ref} from "vue";
+import FormGroup from "@/components/CustomerAttribute/FormGroup.vue";
+import {useCustomerUtils} from "@/composables/customer_utils.js";
+import {useCountriesStore} from "@/stores/countries.js";
+
+const isLoading = ref(false)
+const customerStore = useCustomerStore()
+const countriesStore = useCountriesStore();
+const customerUtils = useCustomerUtils()
+const formErrors = ref({});
+
+/**
+ * @type {{data: Customer | null}}
+ */
+const customer = customerStore.customer;
+
+const identityAttributes = computed(() => {
+  const identityAttributes = [];
+  for (const attribute of customer.data?.attributes) {
+    if (attribute.category === 'identity') {
+      if (typeof formErrors.value[attribute.attribute] !== 'undefined') {
+        attribute['errors'] = formErrors.value[attribute.attribute];
+      }
+      identityAttributes.push(attribute);
+    }
+  }
+  return identityAttributes;
+})
+
+onMounted( () => {
+  if (! customerStore.isLoaded) {
+    customerUtils.refresh();
+  }
+});
+async function update() {
+  isLoading.value = true;
+  await customerUtils.updateProfileIdentity(identityAttributes).catch((e) => {
+    if (e.status === 422) {
+      formErrors.value = e.response.data.errors;
+    } else {
+      console.error(e);
+    }
+  }).finally(() => {
+    isLoading.value = false;
+  });
+}
+
+const showLoading = computed(() => {
+  return isLoading.value || customerStore.isLoaded === false || countriesStore.isLoaded === false;
+})
+</script>
+<template>
+  <div v-show="! showLoading" class="flex-1 flex items-center justify-center p-4 md:p-8">
+    <div class="w-full max-w-xl">
+      <!-- Logo at Top Left (Desktop)  -->
+      <div class="hidden md:block flex items-center justify-center w-full">
+        <a href="javascript:"><img src="/images/logo.png" alt="Tranxfa Logo" class="w-auto max-w-sm"></a>
+      </div>
+      <!-- Form Header -->
+      <h2 class="text-2xl font-semibold text-black mb-4 mt-8">Personal Details</h2>
+      <p class="text-md text-[#B7A3C1] mb-8">Complete your profile by providing some details about you</p>
+      <!-- Form -->
+      <form @submit.prevent="update" class="space-y-6 mt-12">
+        <div v-for="attr in identityAttributes">
+          <FormGroup v-bind:attr="attr" />
+        </div>
+        <button type="submit" class="block w-full bg-brand-700 text-white text-center py-3  rounded-[10px] font-medium hover:bg-brand-800 transition cursor-pointer">Continue</button>
+      </form>
+    </div>
+  </div>
+</template>
