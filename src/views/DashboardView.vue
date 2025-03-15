210e412321e4e878fd2fa2e0a7db5b2aad2e4ba1
Tommy Shelby <dhruv@remitso.com>
2025-03-16 00:18:38 +0530

    refactor: Change TransitionRoot and TransitionChild to use 'div' as the root element for improved structure in DashboardView.vue

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -260,14 +260,14 @@ const recipientCreated = (recipient) => {
         </div>
       </div>
     </main>
-    <TransitionRoot as="template" :show="isCreateRecipientModalOpen">
+    <TransitionRoot as="div" :show="isCreateRecipientModalOpen">
       <Dialog class="relative z-10" @close="isCreateRecipientModalOpen = false">
-        <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <TransitionChild as="div" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
           <div class="fixed inset-0 bg-gray-500/75 transition-opacity" />
         </TransitionChild>
         <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <TransitionChild as="div" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
               <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
                 <AddRecipientWizard v:on:recipient:added="recipientCreated" />
               </DialogPanel>
