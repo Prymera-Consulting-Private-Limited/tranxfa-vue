4bfe062c5aef9796a4ccf8e6922368c51521c81c
Tommy Shelby <dhruv@remitso.com>
2025-03-16 03:41:39 +0530

    feat: Refactor account verification modal handling in CategoryView.vue to support dynamic document types

--- src/views/AccountVerification/CategoryView.vue
+++ src/views/AccountVerification/CategoryView.vue
@@ -5,9 +5,11 @@ import {onMounted, reactive, ref} from "vue";
 import {useCustomerUtils} from "@/composables/customer_utils.js";
 import router from "@/router/index.js";
 import {IdentificationIcon} from "@heroicons/vue/24/outline";
-import snsWebSdk from '@sumsub/websdk';
+
 import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import Spinner from "@/components/Spinner.vue";
+import Sumsub from "@/components/AccountVerification/Provider/Sumsub.vue";
+import System from "@/components/AccountVerification/Provider/System.vue";
 
 const customerStore = useCustomerStore();
 const customerUtils = useCustomerUtils();
@@ -31,72 +33,19 @@ onMounted(async () => {
   }
 });
 
-const openSumsub = ref(false)
-const isSumsubInitialized = ref(false)
+const openSdk = ref(false)
+const isSdkInitialized = ref(false)
+const documentType = ref(null);
 
-/**
- * @param accessToken - access token that you generated on the backend
- */
-async function launchSumsubWebSdk(accessToken) {
-  let snsWebSdkInstance = snsWebSdk
-      .init(
-          accessToken,
-          () => getNewAccessToken()
-      )
-      .withConf({
-        lang: "en",
-        theme: "dark" | "light",
-      })
-      .withOptions({ addViewportTag: false, adaptIframeHeight: true })
-      // see below what kind of messages WebSDK generates
-      .on("idCheck.onStepCompleted", (payload) => {
-        console.log("onStepCompleted", payload);
-      })
-      .on("idCheck.onError", (error) => {
-        console.log("onError", error);
-      })
-      .on("idCheck.onInitialized", () => {
-        isSumsubInitialized.value = true;
-      })
-      .on("idCheck.onApplicantStatusChanged", (payload) => {
-        if (payload.reviewStatus === 'completed') {
-          if (payload.reviewResult.reviewAnswer === ReviewAnswer.GREEN) {
-            if (router.currentRoute.value.query?._utm === 'dashboard-todos') {
-              router.push({name: 'dashboard'});
-            }
-          }
-        } else if (payload.reviewStatus === 'onHold') {
-            if (router.currentRoute.value.query?._utm === 'dashboard-todos') {
-              router.push({name: 'dashboard'});
-            }
-        }
-      })
-      .build();
-
-  snsWebSdkInstance.launch("#sumsub-container");
+async function openAccountVerificationModal (forDocumentType) {
+  documentType.value = forDocumentType;
+  openSdk.value = true;
 }
 
-async function getNewAccessToken(documentCategory, documentType) {
-  let accessToken = null;
-  await customerUtils.getAccountVerificationToken(documentCategory, documentType).then((response) => {
-    accessToken = response.data.token;
-  }).catch((e) => {
-    console.error(e);
-  });
-
-  return accessToken;
-}
-
-async function startSystemVerification(documentType) {
-  const accessToken = await getNewAccessToken(selectedCategory.data, documentType);
-  console.log(accessToken);
-}
-
-async function startSumsubVerification (documentType) {
-  openSumsub.value = true;
-  isSumsubInitialized.value = false;
-  const accessToken = await getNewAccessToken(selectedCategory.data, documentType);
-  await launchSumsubWebSdk(accessToken);
+async function sdkFinalStateReached () {
+    await customerUtils.refresh();
+    openSdk.value = false;
+    await router.push({name: 'accountVerification'});
 }
 </script>
 
@@ -127,8 +76,7 @@ async function startSumsubVerification (documentType) {
                           </dd>
                           <dt class="sr-only">Start Verification</dt>
                           <dd class="mt-3 text-sm text-gray-500">
-                            <a v-if="documentType.api === 'SUMSUB'" @click="startSumsubVerification(documentType)" href="javascript:" class="text-brand-700 font-semibold hover:underline">Start Verification</a>
-                            <a v-else-if="documentType.api === 'SYSTEM'" @click="startSystemVerification(documentType)" href="javascript:" class="text-brand-700 font-semibold hover:underline">Start Verification</a>
+                            <a @click="openAccountVerificationModal(documentType)" href="javascript:" class="text-brand-700 font-semibold hover:underline">Start Verification</a>
                           </dd>
                         </dl>
                       </div>
@@ -147,20 +95,33 @@ async function startSumsubVerification (documentType) {
       </div>
     </main>
   </CustomerLayout>
-  <TransitionRoot as="template" :show="openSumsub">
-    <Dialog class="relative z-10" @close="openSumsub = false">
-      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+  <TransitionRoot as="div" :show="openSdk">
+    <Dialog class="relative z-10" @close="openSdk = false">
+      <TransitionChild as="div" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 bg-gray-500/75 transition-opacity" />
       </TransitionChild>
       <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm lg:max-w-md">
-              <div v-show="! isSumsubInitialized" role="status" class="p-10">
+          <TransitionChild as="div" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:w-sm sm:max-w-sm lg:max-w-md lg:w-md">
+              <div v-show="! isSdkInitialized" role="status" class="p-10">
                 <Spinner class="size-16 mx-auto" />
                 <span class="sr-only">Loading...</span>
               </div>
-              <div v-show="isSumsubInitialized" id="sumsub-container"></div>
+              <Sumsub
+                  v-if="documentType.api === 'SUMSUB'"
+                  v-on:sdkInitialized="isSdkInitialized = true"
+                  v-on:sdkApplicantStatusChanged="sdkFinalStateReached"
+                  v-bind:documentType="documentType"
+                  v-bind:documentCategory="selectedCategory.data"
+              />
+              <System
+                  v-if="documentType.api === 'SYSTEM'"
+                  v-on:sdkInitialized="isSdkInitialized = true"
+                  v-on:sdkApplicantStatusChanged="sdkFinalStateReached"
+                  v-bind:documentType="documentType"
+                  v-bind:documentCategory="selectedCategory.data"
+              />
             </DialogPanel>
           </TransitionChild>
         </div>
