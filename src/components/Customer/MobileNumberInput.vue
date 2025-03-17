22dd049ebffd4a6d3e054292d5d6eba10256b066
Tommy Shelby <dhruv@remitso.com>
2025-03-17 17:09:23 +0530

    feat: add loading overlay to MobileNumberInput component during mobile number update

--- src/components/Customer/MobileNumberInput.vue
+++ src/components/Customer/MobileNumberInput.vue
@@ -34,7 +34,6 @@ function mobileNumberUpdated(updated) {
 }
 
 async function updateMobileNumber() {
-  isLoading.value = true;
   isSaving.value = true;
   await customerUtils.updateMobileNumber(mobile.country, mobile.number).catch((e) => {
     if (e.status === 422) {
@@ -43,7 +42,6 @@ async function updateMobileNumber() {
       console.error(e);
     }
   }).finally(() => {
-    isLoading.value = false;
     isSaving.value = false;
   });
 }
@@ -54,15 +52,18 @@ const showLoading = computed(() => {
 </script>
 
 <template>
-  <div class="flex-1 flex items-center justify-center p-4 md:p-8">
-    <div class="w-full max-w-xl">
+  <div class="relative flex-1 flex items-center justify-center p-4 md:p-8">
+    <div v-if="showLoading" class="absolute inset-0 flex items-center justify-center bg-white/75 z-10">
+      <i class="pi pi-spin pi-spinner text-5xl text-brand-700"></i>
+    </div>
+    <div v-show="! showLoading || isSaving" class="w-full max-w-xl">
       <div class="hidden md:block flex items-center justify-center w-full">
         <a href="javascript:"><img src="/images/logo.png" alt="Tranxfa Logo" class="w-auto max-w-sm"></a>
       </div>
       <h2 class="text-2xl font-semibold text-black mb-4 text-left">Enter Your Mobile Number</h2>
       <p class="text-md text-[#B7A3C1] mb-8 text-left">Please provide your mobile number along with the ISD code to continue.</p>
       <!-- Form -->
-      <form v-show="! showLoading" @submit.prevent="updateMobileNumber" class="space-y-6 mt-12">
+      <form @submit.prevent="updateMobileNumber" class="space-y-6 mt-12">
         <MobileNumberInput v-bind:mobile="mobile" v-bind:errors="errors" v-on:update:mobileNumberUpdated="mobileNumberUpdated" />
         <button :disabled="showLoading || isSaving" :class="[{'opacity-70': isLoading || isSaving}]" type="submit" class="block w-full bg-brand-700 text-white text-center py-3  rounded-[10px] font-medium hover:bg-brand-800 transition cursor-pointer">
           <template v-if="isSaving">
