19de2f4853783d2ef1e42452bd91bc7afdd9a346
Tommy Shelby <dhruv@remitso.com>
2025-03-19 16:25:42 +0530

    feat: add padding to AddRecipientWizard component in DashboardView and Index

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -269,7 +269,7 @@ const recipientCreated = (recipient) => {
           <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <TransitionChild as="div" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
               <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
-                <AddRecipientWizard v:on:recipient:added="recipientCreated" />
+                <AddRecipientWizard class="p-6 sm:px-8" v:on:recipient:added="recipientCreated" />
               </DialogPanel>
             </TransitionChild>
           </div>
