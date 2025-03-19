bf458d05a9eadf123242e42a8cd1c89b37c0c663
Tommy Shelby <dhruv@remitso.com>
2025-03-20 00:01:57 +0530

    feat: enhance Progress component to include step IDs and improve rendering logic for better user experience

--- src/components/Transaction/Progress.vue
+++ src/components/Transaction/Progress.vue
@@ -1,7 +1,8 @@
 <script setup>
-import {CheckIcon} from "@heroicons/vue/20/solid";
+import {CheckIcon, PaperAirplaneIcon, XMarkIcon, UserIcon} from "@heroicons/vue/24/outline";
 import {computed} from "vue";
 import TransactionQuote from "@/models/transaction_quote.js";
+import FlagIcon from "vue3-flag-icons";
 
 const props = defineProps({
   currentStep: {
@@ -16,55 +17,59 @@ const props = defineProps({
 
 const steps = [
   {
-    name: 'Prepare Your Transfer',
+    id: 'checkRecipients',
+    name: 'Prepare your transfer',
     description: 'Get started by entering the amount and selecting your transfer method for a smooth transaction.',
-    href: '#',
-    action: ['prepare']
   },
   {
+    id: 'addRecipient',
     name: 'Add Recipient Details',
     description: 'Tell us who you’re sending money to by providing their name and transfer information.',
-    href: '#',
-    action: ['addRecipient', 'selectRecipient']
   },
-  // {
-  //   name: 'Provide Your Address',
-  //   description: 'For security and compliance, we need your address details before proceeding.',
-  //   href: '#',
-  //   status: 'upcoming'
-  // },
-  // {
-  //   name: 'Verify Your Identity',
-  //   description: 'A quick identity check ensures your transfer is safe and secure.',
-  //   href: '#',
-  //   status: 'upcoming'
-  // },
   {
+    id: 'selectRecipient',
+    name: 'Choose your recipient',
+    description: 'Tell us who you’re sending money to by providing their name and transfer information.',
+  },
+  {
+    id: 'provideAddress',
+    name: 'Provide Your Address',
+    description: 'For security and compliance, we need your address details before proceeding.',
+  },
+  {
+    id: 'verifyIdentity',
+    name: 'Verify Your Identity',
+    description: 'A quick identity check ensures your transfer is safe and secure.',
+  },
+  {
+    id: 'confirm',
     name: 'Review & Confirm',
     description: 'Double-check all details before finalizing your transfer.',
-    href: '#',
-    action: ['confirm']
   },
-  // {
-  //   name: 'Make Payment',
-  //   description: 'Complete your transfer by choosing a payment method and sending the funds.',
-  //   href: '#',
-  // }
+  {
+    id: 'makePayment',
+    name: 'Make Payment',
+    description: 'Complete your transfer by choosing a payment method and sending the funds.',
+  }
 ];
 
 const progress = computed(() => {
+  const progress = [];
   for (let i = 0; i < steps.length; i++) {
-    const cursor = steps.findIndex((step) => step.action.includes(props.currentStep));
-    if (steps[i].action.includes('selectRecipient')) {
-      steps[i].name = props.quote?.recipients?.length > 0 ? 'Select Recipient Details' : 'Add Recipient Details';
+    const step = steps[i];
+    if (step.id === 'checkRecipients' && props.quote) {
+      step.name = `Transfer to ${props.quote?.payoutCountry?.commonName}`;
+
     }
+    const cursor = steps.findIndex((step) => props.currentStep === step.id);
     if (i < cursor) {
-      steps[i].status = 'complete';
+      step.status = 'complete';
     } else if (i === cursor) {
-      steps[i].status = 'current';
+      step.status = 'current';
     } else {
-      steps[i].status = 'upcoming';
+      step.status = 'upcoming';
     }
+    progress.push(step);
   }
 
   return steps;
@@ -73,50 +78,53 @@ const progress = computed(() => {
 <template>
   <nav aria-label="Progress">
     <ol role="list" class="overflow-hidden">
-      <li v-for="(step, stepIdx) in progress" :key="step.name" :class="[stepIdx !== steps.length - 1 ? 'pb-10' : '', 'relative']">
-        <template v-if="step.status === 'complete'">
-          <div v-if="stepIdx !== steps.length - 1" class="absolute top-4 left-4 mt-0.5 -ml-px h-full w-0.5 bg-purple-600" aria-hidden="true" />
-          <a :href="step.href" class="group relative flex items-start">
-            <span class="flex h-9 items-center">
-              <span class="relative z-10 flex size-8 items-center justify-center rounded-full bg-purple-600 group-hover:bg-purple-800">
-                <CheckIcon class="size-5 text-white" aria-hidden="true" />
-              </span>
-            </span>
-            <span class="ml-4 flex min-w-0 flex-col">
-              <span class="text-sm font-medium">{{ step.name }}</span>
-              <span class="text-sm text-gray-500">{{ step.description }}</span>
-            </span>
-          </a>
-        </template>
-        <template v-else-if="step.status === 'current'">
-          <div v-if="stepIdx !== steps.length - 1" class="absolute top-4 left-4 mt-0.5 -ml-px h-full w-0.5 bg-gray-300" aria-hidden="true" />
-          <a :href="step.href" class="group relative flex items-start" aria-current="step">
-            <span class="flex h-9 items-center" aria-hidden="true">
-              <span class="relative z-10 flex size-8 items-center justify-center rounded-full border-2 border-purple-600 bg-white">
-                <span class="size-2.5 rounded-full bg-purple-600" />
-              </span>
-            </span>
-            <span class="ml-4 flex min-w-0 flex-col">
-              <span class="text-sm font-medium text-purple-600">{{ step.name }}</span>
-              <span class="text-sm text-gray-500">{{ step.description }}</span>
-            </span>
-          </a>
-        </template>
-        <template v-else>
-          <div v-if="stepIdx !== steps.length - 1" class="absolute top-4 left-4 mt-0.5 -ml-px h-full w-0.5 bg-gray-300" aria-hidden="true" />
-          <a :href="step.href" class="group relative flex items-start">
-            <span class="flex h-9 items-center" aria-hidden="true">
-              <span class="relative z-10 flex size-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400">
-                <span class="size-2.5 rounded-full bg-transparent group-hover:bg-gray-300" />
-              </span>
-            </span>
-            <span class="ml-4 flex min-w-0 flex-col">
-              <span class="text-sm font-medium text-gray-500">{{ step.name }}</span>
-              <span class="text-sm text-gray-500">{{ step.description }}</span>
-            </span>
-          </a>
-        </template>
-      </li>
+      <template v-for="(step, stepIdx) in progress" :key="step.name">
+        <li v-if="step.status !== 'completed' && step.id !== 'addRecipient'" :class="[stepIdx !== steps.length - 1 ? 'pb-10' : '', 'relative']">
+          <template v-if="step.status === 'complete'">
+            <div v-if="stepIdx !== steps.length - 1" class="absolute top-4 left-4 mt-0.5 -ml-px h-full w-0.5 bg-purple-600" aria-hidden="true" />
+            <div class="group relative flex items-start">
+              <div class="flex h-9 items-center">
+                <div :class="{'bg-purple-600 group-hover:bg-purple-800' : stepIdx !== 0 || !quote}" class="relative z-10 flex size-8 items-center justify-center rounded-full">
+                  <FlagIcon v-if="stepIdx === 0 && quote" :code="quote.payoutCountry.iso2Alpha.toLowerCase()" circle size="30"  />
+                  <CheckIcon v-else class="size-5 text-white" aria-hidden="true" />
+                </div>
+              </div>
+              <div class="ml-4 flex min-w-0 flex-col">
+                <div class="text-sm font-medium">{{ step.name }}</div>
+                <p class="mt-1 text-sm text-gray-500">{{ step.description }}</p>
+              </div>
+            </div>
+          </template>
+          <template v-else-if="step.status === 'current'">
+            <div v-if="stepIdx !== steps.length - 1" class="absolute top-4 left-4 mt-0.5 -ml-px h-full w-0.5 bg-gray-300" aria-hidden="true" />
+            <div class="group relative flex items-start" aria-current="step">
+              <div class="flex h-9 items-center" aria-hidden="true">
+                <div class="relative z-10 flex size-8 items-center justify-center rounded-full border-2 border-purple-600 bg-white">
+                  <div class="size-2.5 rounded-full bg-purple-600" />
+                </div>
+              </div>
+              <div class="ml-4 flex min-w-0 flex-col">
+                <div class="text-sm font-medium text-purple-600">{{ step.name }}</div>
+                <p class="text-sm text-gray-500">{{ step.description }}</p>
+              </div>
+            </div>
+          </template>
+          <template v-else>
+            <div v-if="stepIdx !== steps.length - 1" class="absolute top-4 left-4 mt-0.5 -ml-px h-full w-0.5 bg-gray-300" aria-hidden="true" />
+            <div class="group relative flex items-start">
+              <div class="flex h-9 items-center" aria-hidden="true">
+                <div class="relative z-10 flex size-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400">
+                  <div class="size-2.5 rounded-full bg-transparent group-hover:bg-gray-300" />
+                </div>
+              </div>
+              <div class="ml-4 flex min-w-0 flex-col">
+                <div class="text-sm font-medium text-gray-500">{{ step.name }}</div>
+                <p class="text-sm text-gray-500">{{ step.description }}</p>
+              </div>
+            </div>
+          </template>
+        </li>
+      </template>
     </ol>
   </nav>
 </template>
