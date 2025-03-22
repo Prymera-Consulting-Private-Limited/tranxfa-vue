5a583e00388e8844aaebf0e8ab288dfe318c94fa
Tommy Shelby <dhruv@remitso.com>
2025-03-22 16:53:59 +0530

    refactor: simplify CategoryView by replacing modal with DocumentTypeItem component

--- src/views/AccountVerification/CategoryView.vue
+++ src/views/AccountVerification/CategoryView.vue
@@ -1,15 +1,10 @@
 <script setup>
 import CustomerLayout from "@/components/CustomerLayout.vue";
 import {useCustomerStore} from "@/stores/customer.js";
-import {onMounted, reactive, ref} from "vue";
+import {onMounted, reactive} from "vue";
 import {useCustomerUtils} from "@/composables/customer_utils.js";
 import router from "@/router/index.js";
-import {IdentificationIcon} from "@heroicons/vue/24/outline";
-
-import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
-import Spinner from "@/components/Spinner.vue";
-import Sumsub from "@/components/AccountVerification/Provider/Sumsub.vue";
-import System from "@/components/AccountVerification/Provider/System.vue";
+import DocumentTypeItem from "@/components/AccountVerification/DocumentTypeItem.vue";
 
 const customerStore = useCustomerStore();
 const customerUtils = useCustomerUtils();
@@ -33,19 +28,8 @@ onMounted(async () => {
   }
 });
 
-const openSdk = ref(false)
-const isSdkInitialized = ref(false)
-const documentType = ref(null);
-
-async function openAccountVerificationModal (forDocumentType) {
-  documentType.value = forDocumentType;
-  openSdk.value = true;
-}
-
-async function sdkFinalStateReached () {
-    await customerUtils.refresh();
-    openSdk.value = false;
-    await router.push({name: 'accountVerification'});
+const finalStateReached = async () => {
+  await router.push({name: 'accountVerification'});
 }
 </script>
 
@@ -66,20 +50,11 @@ async function sdkFinalStateReached () {
                 <div class="mt-6 border-t border-b border-gray-200 py-6 w-full">
                   <ul v-if="selectedCategory.data?.documentTypes?.length > 0" role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                     <li v-for="documentType in selectedCategory.data.documentTypes" :key="documentType.id" class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow-sm">
-                      <div class="flex flex-1 flex-col p-8">
-                        <IdentificationIcon class="mx-auto size-16 shrink-0 rounded-full text-purple-700" />
-                        <h3 class="mt-6 text-sm font-medium text-gray-900">{{ documentType.title }}</h3>
-                        <dl v-if="documentType.description" class="mt-1 flex grow flex-col justify-between">
-                          <dt class="sr-only">Information</dt>
-                          <dd class="mt-3 text-sm text-gray-500">
-                            <p>{{ documentType.description }}</p>
-                          </dd>
-                          <dt class="sr-only">Start Verification</dt>
-                          <dd class="mt-3 text-sm text-gray-500">
-                            <a @click="openAccountVerificationModal(documentType)" href="javascript:" class="text-brand-700 font-semibold hover:underline">Start Verification</a>
-                          </dd>
-                        </dl>
-                      </div>
+                      <DocumentTypeItem
+                          v-bind:documentType="documentType"
+                          v-bind:documentCategory="selectedCategory.data"
+                          v-on:sdkFinalStateReached=""
+                      />
                     </li>
                   </ul>
                 </div>
@@ -95,37 +70,4 @@ async function sdkFinalStateReached () {
       </div>
     </main>
   </CustomerLayout>
-  <TransitionRoot as="div" :show="openSdk">
-    <Dialog class="relative z-10" @close="openSdk = false">
-      <TransitionChild as="div" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
-        <div class="fixed inset-0 bg-gray-500/75 transition-opacity" />
-      </TransitionChild>
-      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <TransitionChild as="div" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:w-sm sm:max-w-sm lg:max-w-md lg:w-md">
-              <div v-show="! isSdkInitialized" role="status" class="p-10">
-                <Spinner class="size-16 mx-auto" />
-                <span class="sr-only">Loading...</span>
-              </div>
-              <Sumsub
-                  v-if="documentType.api === 'SUMSUB'"
-                  v-on:sdkInitialized="isSdkInitialized = true"
-                  v-on:sdkApplicantStatusChanged="sdkFinalStateReached"
-                  v-bind:documentType="documentType"
-                  v-bind:documentCategory="selectedCategory.data"
-              />
-              <System
-                  v-if="documentType.api === 'SYSTEM'"
-                  v-on:sdkInitialized="isSdkInitialized = true"
-                  v-on:sdkApplicantStatusChanged="sdkFinalStateReached"
-                  v-bind:documentType="documentType"
-                  v-bind:documentCategory="selectedCategory.data"
-              />
-            </DialogPanel>
-          </TransitionChild>
-        </div>
-      </div>
-    </Dialog>
-  </TransitionRoot>
 </template>
