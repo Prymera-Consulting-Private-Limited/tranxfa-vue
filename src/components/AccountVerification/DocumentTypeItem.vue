997ac940ef9a22ffc603eba805f633c6e2d67c8b
Tommy Shelby <dhruv@remitso.com>
2025-03-23 02:07:46 +0530

    fix: update sdkFinalStateReached function to emit event and remove redundant call in DocumentTypeItem component

--- src/components/AccountVerification/DocumentTypeItem.vue
+++ src/components/AccountVerification/DocumentTypeItem.vue
@@ -34,9 +34,8 @@ const emit = defineEmits([
 ])
 
 async function sdkFinalStateReached () {
-  await customerUtils.refresh();
+  await emit('sdkFinalStateReached');
   openSdk.value = false;
-  emit('sdkFinalStateReached');
 }
 
 </script>
@@ -65,7 +64,7 @@ async function sdkFinalStateReached () {
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <TransitionChild as="div" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
             <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:w-sm sm:max-w-sm lg:max-w-md lg:w-md">
-              <button class="sr-only" aria-hidden="true"></button>
+              <button class="sr-only"></button>
               <div v-show="! isSdkInitialized" role="status" class="p-10">
                 <Spinner class="size-16 mx-auto" />
                 <span class="sr-only">Loading...</span>
