1bb8f7c91b63e36a57d3e05a586b59ed91a67708
Tommy Shelby <dhruv@remitso.com>
2025-03-23 15:42:57 +0530

    refactor: simplify color utility functions and enhance style value retrieval

--- src/composables/color_utils.js
+++ src/composables/color_utils.js
@@ -1,58 +1,15 @@
 export function useColorUtils () {
 
-    /**
-     * @param {String} prefix
-     * @param {String} color
-     * @param {Number} variable
-     * @returns {String}
-     */
-    const getClass = (prefix, color, variable) => {
-        return `${prefix}-${color}-${variable}`;
+    const getStyleValue = (value, variable) => {
+        return `var(--color-${value}-${variable})`
     }
 
-    /**
-     * @param {String} prefix
-     * @param {String} color
-     * @param {Number} variable
-     * @param {Number} opacity
-     * @returns {String}
-     */
-    const getClassWithOpacity = (prefix, color, variable, opacity) => {
-        return `${prefix}-${color}-${variable}/${opacity}`;
-    }
-
-    const getBgClass = (color, variable) => {
-        return getClass('bg', color, variable);
-    }
-
-    const getBorderClass = (color, variable) => {
-        return getClass('border', color, variable);
-    }
-
-    const getTextClass = (color, variable) => {
-        return getClass('text', color, variable);
-    }
-
-    const getBgClassWithOpacity = (color, variable, opacity) => {
-        return getClassWithOpacity('bg', color, variable, opacity);
-    }
-
-    const getBorderClassWithOpacity = (color, variable, opacity) => {
-        return getClassWithOpacity('border', color, variable, opacity);
-    }
-
-    const getTextClassWithOpacity = (color, variable, opacity) => {
-        return getClassWithOpacity('text', color, variable, opacity);
+    const getStyleValueWithOpacityValue = (value, variable, opacity) => {
+        return `var(--color-${value}-${variable}/${opacity})`
     }
 
     return {
-        getClass,
-        getClassWithOpacity,
-        getBgClass,
-        getBorderClass,
-        getTextClass,
-        getBgClassWithOpacity,
-        getBorderClassWithOpacity,
-        getTextClassWithOpacity,
+        getStyleValue,
+        getStyleValueWithOpacityValue,
     };
 }
