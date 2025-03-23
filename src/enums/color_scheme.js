ed83a5044fefe74a3b10a00546840d0ee289b185
Tommy Shelby <dhruv@remitso.com>
2025-03-23 14:16:31 +0530

    feat: add ColorScheme module with predefined color constants

---
+++ src/enums/color_scheme.js
@@ -0,0 +1,10 @@
+const ColorScheme = Object.freeze({
+    GRAY: 'gray',
+    BLUE: 'blue',
+    YELLOW: 'yellow',
+    GREEN: 'green',
+    ORANGE: 'orange',
+    RED: 'red',
+});
+
+export default ColorScheme;
