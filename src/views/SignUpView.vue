c00f65e00b36ebf860b5b38fd04fd8b76fb98b72
Tommy Shelby <dhruv@remitso.com>
2025-03-25 19:50:25 +0530

    feat: update link styling in SignUpView for consistent hover effect

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -212,7 +212,7 @@ async function register() {
             </form>
 
             <p class="text-sm text-gray-500 mt-10 text-center">
-              Already have an account? <router-link class="text-brand-700 hover:text-brand-800 hover:underline" :to="{name: 'signIn'}">Sign in instead</router-link>
+              Already have an account? <router-link class="text-brand-700 hover:text-brand-700 hover:underline" :to="{name: 'signIn'}">Sign in instead</router-link>
             </p>
           </div>
         </div>
