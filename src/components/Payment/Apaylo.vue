bd19b0b81f52368728a022bfba405e31aae407a8
Tommy Shelby <dhruv@remitso.com>
2025-03-25 16:45:04 +0530

    feat: add shared reference to payment transaction and update button styles in Apaylo.vue

--- src/components/Payment/Apaylo.vue
+++ src/components/Payment/Apaylo.vue
@@ -18,6 +18,7 @@ onMounted(async () => {
   Echo.channel(`client-payment.${props.transaction.payment.id}`)
       .listen('PaymentTransactionStateUpdated', (e) => {
         props.transaction.payment.state = PaymentTransactionState.getInstance(e.state);
+        props.transaction.payment.sharedReference = e.shared_reference;
       });
 })
 
@@ -44,11 +45,11 @@ const redirected = async () => {
     <p class="text-lg text-gray-600 mb-6">Your payment has been successfully received.</p>
   </template>
 
-  <template v-if="transaction.payment.state.code === PaymentState.PENDING">
+  <template v-else-if="transaction.payment.state.code === PaymentState.PENDING">
     <AwaitingPending class="-mt-10" />
     <h2 class="text-xl font-semibold text-gray-900 mb-5 -mt-10">Complete Your Payment</h2>
     <p class="text-base text-gray-600 mb-6">Your transaction is pending. To proceed, please click the button below and complete the payment.</p>
-    <a @click="redirected" :href="`https://gateway-web.fit.interac.ca/acceptPaymentRequest.do?rID=${transaction.payment.sharedReference}`" class="mt-6 px-4 md:px-6 lg:px-8 bg-blue-600 text-white text-center py-3 rounded-md font-medium hover:bg-blue-700 transition cursor-pointer text-sm" target="_blank">Proceed to Payment</a>
+    <a @click="redirected" :href="`https://gateway-web.fit.interac.ca/acceptPaymentRequest.do?rID=${transaction.payment.sharedReference}`" class="mt-6 px-4 md:px-6 lg:px-8 bg-purple-600 text-white text-center py-3 rounded-md font-medium hover:bg-purple-700 transition cursor-pointer text-sm outline-none ring-0" target="_blank">Proceed to Payment</a>
   </template>
 
   <template v-else>
