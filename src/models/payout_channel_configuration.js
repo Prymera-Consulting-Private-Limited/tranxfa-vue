19d5107ac9f2a935da11d54afff33773a3978081
Tommy Shelby <dhruv@remitso.com>
2025-03-31 23:16:08 +0530

    feat: remove askForMiddleName property from PayoutChannelConfiguration

--- src/models/payout_channel_configuration.js
+++ src/models/payout_channel_configuration.js
@@ -4,15 +4,9 @@ class PayoutChannelConfiguration {
      */
     recipientType = null;
 
-    /**
-     * @type {boolean}
-     */
-    askForMiddleName = false;
-
     static getInstance(data) {
         const payoutChannelConfiguration = new PayoutChannelConfiguration();
         payoutChannelConfiguration.recipientType = data.recipient_type ?? null;
-        payoutChannelConfiguration.askForMiddleName = data.ask_for_middle_name ?? false;
         return payoutChannelConfiguration;
     }
 }
