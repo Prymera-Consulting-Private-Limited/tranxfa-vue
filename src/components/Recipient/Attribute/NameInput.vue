806b4a3634debb15ab595fb4aa471261cd178ef7
Tommy Shelby <dhruv@remitso.com>
2025-03-31 23:17:17 +0530

    feat: rename input components for consistency and update event handling

---
+++ src/components/Recipient/Attribute/NameInput.vue
@@ -0,0 +1,23 @@
+<script setup>
+import TextInput from "@/components/Recipient/Attribute/TextInput.vue";
+import PayoutChannelAttribute from "@/models/payout_channel_attribute.js";
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  attribute: {
+    type: PayoutChannelAttribute,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['recipient:input:updated']);
+const nameUpdated = (value) => {
+  emit('recipient:input:updated', value, props.attribute);
+}
+</script>
+
+<template>
+  <TextInput v-on:recipient:input:updated="nameUpdated" :id="id" class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
+</template>
---
+++ src/components/Recipient/Attribute/SecondNameInput.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TextInput from "@/components/Recipient/Attribute/TextInput.vue";
+import PayoutChannelAttribute from "@/models/payout_channel_attribute.js";
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  attribute: {
+    type: PayoutChannelAttribute,
+    required: true,
+  },
+})
+
+const secondNameUpdated = (value) => {
+  emit('recipient:input:updated', value, props.attribute);
+}
+</script>
+
+<template>
+  <TextInput v-on:recipient:input:updated="secondNameUpdated" :id="id" class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
+</template>
