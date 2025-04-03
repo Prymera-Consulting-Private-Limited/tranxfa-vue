549d585da2419cdbc641df87982f21fef04ed7ae
Tommy Shelby <dhruv@remitso.com>
2025-04-03 19:41:20 +0530

    feat: enhance mobile number input component with customer data integration and inputmode for numeric input

--- src/components/CustomerAttribute/MobileNumberInput.vue
+++ src/components/CustomerAttribute/MobileNumberInput.vue
@@ -1,24 +1,28 @@
 <script setup>
 import IsdCodeInput from "@/components/IsdCodeInput.vue";
 import {reactive, watch} from "vue";
+import {useCustomerStore} from "@/stores/customer.js";
+
+const customerStore = useCustomerStore();
+
+const customer = customerStore.customer;
 
 const props = defineProps({
   mobile: {
-    type: Object({
-      country: String,
-      number: String,
+    type: Object,
+    required: false,
+    default: () => ({
+      country: null,
+      number: null,
     }),
-    required: true,
   },
   errors: Object,
 });
 const emit = defineEmits(['update:mobileNumberUpdated']);
 const mobileNumber = reactive({
-  country: '',
-  number: '',
+  country: props.mobile?.country || customer.data?.account?.mobileNumberCountry?.id || customer.data?.country?.id || null,
+  number: props.mobile?.number || customer.data?.account?.mobileNumber || null,
 })
-mobileNumber.country = props.mobile?.country || null;
-mobileNumber.number = props.mobile?.number || null;
 function updateIsdCode(updated) {
   mobileNumber.country = updated?.id;
 }
@@ -37,7 +41,7 @@ watch(mobileNumber, () => {
   <div>
     <label for="mobile-number" :class="[errors?.mobile_number?.length > 0 ? 'text-red-700' : 'text-brand-700']" class="block text-sm font-medium mb-0">Mobile Number</label>
     <p class="mt-2 mb-3 text-gray-400 text-sm"></p>
-    <input id="mobile-number" type="tel" v-model="mobileNumber.number" class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
+    <input autofocus inputmode="number" id="mobile-number" type="tel" v-model="mobileNumber.number" class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none" />
     <p v-if="errors?.mobile_number?.length > 0" class="mt-2 text-sm text-red-600 dark:text-red-500">{{ errors.mobile_number[0] }}</p>
   </div>
 </template>
