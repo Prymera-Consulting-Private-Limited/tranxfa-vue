2b39a59536dea5f3347c3a2a7fbcd3c49b9e41e3
Tommy Shelby <dhruv@remitso.com>
2025-04-12 15:51:20 +0530

    feat(router): add NProgress for page loading indicators

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignUpView from "@/views/SignUpView.vue";
 import SignInView from "@/views/SignInView.vue";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -134,8 +136,19 @@ const router = createRouter({
   ],
 })
 
+NProgress.configure({ showSpinner: false, trickleSpeed: 200 })
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
 router.beforeEach((to, from) => {
   document.title = to.meta?.title ?? 'Default Title'
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
