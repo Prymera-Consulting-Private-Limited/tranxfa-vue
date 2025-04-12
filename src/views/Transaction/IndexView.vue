522baa246bf0f8aade193f8abcf9e72df1af6c94
Tommy Shelby <dhruv@remitso.com>
2025-04-12 15:28:33 +0530

    feat: refactor transaction display by creating ListItem component for better code organization

--- src/views/Transaction/IndexView.vue
+++ src/views/Transaction/IndexView.vue
@@ -5,15 +5,11 @@ import {computed, onMounted, ref} from "vue";
 import {useTransactionUtils} from "@/composables/transaction_utils.js";
 import Transaction from "@/models/transaction.js";
 import {useTimeUtils} from "@/composables/time_utils.js";
-import {useColorUtils} from "@/composables/color_utils.js";
-import TransactionStateIcon from "@/enums/transaction_state_icon.js";
-import TransactionState from "@/enums/transaction_state.js";
-import moment from "moment";
 import {BanknotesIcon} from "@heroicons/vue/24/outline";
+import ListItem from "@/components/Transaction/ListItem.vue";
 
 const transactionUtils = useTransactionUtils();
 const timeUtils = useTimeUtils();
-const colorUtils = useColorUtils();
 
 const data = ref(null);
 const isLoading = ref(true);
@@ -73,44 +69,7 @@ const transactions = computed(() => {
                 <ul role="list" class="divide-y divide-gray-100">
                   <template v-for="(transaction, i) in transactions" :key="transaction.data.id">
                     <router-link :class="{'rounded-t-lg': i === 0}" as="li" :to="{name: 'viewTransaction', params: {transactionId: transaction.data.id}}" class="flex justify-between gap-x-6 py-5 px-6 sm:px-8 cursor-pointer hover:bg-gray-50">
-                      <div :class="{'opacity-75': transaction.data.state.code === TransactionState.CANCELLED}" class="flex min-w-0 gap-x-4">
-                    <span class="inline-flex size-11 items-center justify-center border border-1 rounded-full" :style="{
-                       backgroundColor: colorUtils.getStyleValue(transaction.data.state.colorScheme, 50),
-                       borderColor: colorUtils.getStyleValue(transaction.data.state.colorScheme, 600),
-                     }">
-                        <component :style="{
-                         color: colorUtils.getStyleValue(transaction.data.state.colorScheme, 600),
-                       }" :is="TransactionStateIcon[transaction.data.state.code]" class="size-6" />
-                    </span>
-                        <div class="min-w-0 flex-auto">
-                          <div class="text-sm/6 font-semibold text-gray-900">{{ transaction.data.localAmountCurrencyPrefixed }} to <span class="text-purple-700">{{ transaction.data.recipient.wholeName }}</span></div>
-                          <div class="text-xs/5 text-gray-800 flex justify-center items-center gap-x-1.5">
-                            Sent {{ transaction.data.foreignAmountCurrencyPrefixed }} via {{ transaction.data.payoutMethod.title }}
-                            <span class="flex justify-center items-center text-xs/5 text-gray-500">
-                          <abbr :title="moment(transaction.data.createdAt)">{{ transaction.niceTime }}</abbr>
-                        </span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                        <div class="mt-1 flex items-center gap-x-1.5">
-                          <p :style="{
-                         color: colorUtils.getStyleValue(transaction.data.state.colorScheme, 600),
-                       }" class="text-xs/5">
-                        <span :style="{
-                         backgroundColor: colorUtils.getStyleValue(transaction.data.state.colorScheme, 50),
-                         '--tw-ring-color': colorUtils.getStyleValue(transaction.data.state.colorScheme, 200),
-                       }" class="inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset">
-                          <svg :style="{
-                             fill: colorUtils.getStyleValue(transaction.data.state.colorScheme, 600),
-                           }" class="size-1.5" viewBox="0 0 6 6" aria-hidden="true">
-                            <circle cx="3" cy="3" r="3" />
-                          </svg>
-                          {{ transaction.data.state.label }}
-                        </span>
-                          </p>
-                        </div>
-                      </div>
+                      <ListItem v-bind:niceTime="transaction.niceTime" v-bind:transaction="transaction.data" />
                     </router-link>
                   </template>
                 </ul>
