33f8236e9a5cd9956a013614b3930a1bb8f1b681
Tommy Shelby <dhruv@remitso.com>
2025-04-12 15:43:11 +0530

    fix(ListItem): align text justification for transaction details

--- src/components/Transaction/ListItem.vue
+++ src/components/Transaction/ListItem.vue
@@ -30,7 +30,7 @@ const props = defineProps({
     </span>
     <div class="min-w-0 flex-auto">
       <div class="text-sm/6 font-semibold text-gray-900">{{ transaction.localAmountCurrencyPrefixed }} to <span class="text-purple-700">{{ transaction.recipient.wholeName }}</span></div>
-      <div class="text-xs/5 text-gray-800 flex justify-center items-center gap-x-1.5">
+      <div class="text-xs/5 text-gray-800 flex justify-start items-center gap-x-1.5">
         Sent {{ transaction.foreignAmountCurrencyPrefixed }} via {{ transaction.payoutMethod.title }}
         <span class="flex justify-center items-center text-xs/5 text-gray-500">
           <abbr :title="moment(transaction.createdAt)">{{ niceTime }}</abbr>
