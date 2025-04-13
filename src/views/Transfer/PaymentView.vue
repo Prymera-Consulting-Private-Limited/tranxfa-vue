fe3ab9049d7515fa0a6474c169135cdb8b9f7db7
Tommy Shelby <dhruv@remitso.com>
2025-04-13 22:56:03 +0530

    refactor(PaymentView): adjust padding for improved layout and visual consistency

--- src/views/Transfer/PaymentView.vue
+++ src/views/Transfer/PaymentView.vue
@@ -54,7 +54,7 @@ onMounted(async () => {
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
             <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
-              <div>
+              <div class="p-8 sm:pb-6">
                 <div class="mt-3 text-center sm:mt-5">
                   <div v-if="transaction" class="text-center">
                     <Apaylo v-if="transaction.payment.paymentProvider.code === 'APAYLO'" v-bind:transaction="transaction"  />
