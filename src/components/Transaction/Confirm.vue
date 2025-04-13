952e3b3c77644e7f39a63952a3c16e3f695f101a
Tommy Shelby <dhruv@remitso.com>
2025-04-13 22:51:43 +0530

    refactor(Confirm): streamline review item rendering for improved maintainability

--- src/components/Transaction/Confirm.vue
+++ src/components/Transaction/Confirm.vue
@@ -14,129 +14,100 @@ import {
 } from "@heroicons/vue/24/outline";
 import RecipientDataType from "@/enums/recipient_data_type.js";
 
-defineProps({
+const props = defineProps({
   quote: {
     type: TransactionQuote,
     required: true,
   }
 })
+
+const reviewItems = [
+  {
+    icon: UserCircleIcon,
+    label: 'Recipient',
+    value: props.quote.recipient.wholeName,
+  },
+  {
+    icon: FlagIcon,
+    label: 'Payout Country',
+    value: props.quote.payoutCountry.commonName,
+  },
+  {
+    icon: TruckIcon,
+    label: 'Payout Method',
+    value: props.quote.payoutMethod.title,
+  }
+];
+for (let i = 0; i < props.quote.recipient.accountDetailHashMap.length; i++) {
+  const accountDetailHashmap = props.quote.recipient.accountDetailHashMap[i];
+  if (accountDetailHashmap.type === RecipientDataType.ACCOUNT_HOLDER_NAME) {
+    reviewItems.push({
+      icon: UserCircleIcon,
+      label: accountDetailHashmap.key,
+      value: accountDetailHashmap.value,
+    });
+  } else if (accountDetailHashmap.type === RecipientDataType.DELIVERY_OPTION) {
+    reviewItems.push({
+      icon: BuildingLibraryIcon,
+      label: accountDetailHashmap.key,
+      value: accountDetailHashmap.value,
+    });
+  } else if (accountDetailHashmap.type === RecipientDataType.ACCOUNT_NUMBER) {
+    reviewItems.push({
+      icon: DocumentCurrencyDollarIcon,
+      label: accountDetailHashmap.key,
+      value: accountDetailHashmap.value,
+    });
+  }
+}
+reviewItems.push({
+  icon: PaperAirplaneIcon,
+  label: 'Sending Amount',
+  value: props.quote.localAmountCurrencyPrefixed,
+});
+reviewItems.push({
+  icon: BanknotesIcon,
+  label: 'Exchange Rate',
+  value: props.quote.exchangeRateFormatted,
+});
+reviewItems.push({
+  icon: WalletIcon,
+  label: 'Recipient Gets',
+  value: props.quote.foreignAmountCurrencyPrefixed,
+});
+reviewItems.push({
+  icon: PlusIcon,
+  label: 'Fees',
+  value: props.quote.baseFeesCurrencyPrefixed,
+});
+reviewItems.push({
+  icon: CalculatorIcon,
+  label: 'Subtotal',
+  value: props.quote.localAmountCurrencyPrefixed,
+});
+reviewItems.push({
+  icon: WalletIcon,
+  label: 'Total Due',
+  value: props.quote.localAmountCurrencyPrefixed,
+});
 </script>
 
 <template>
   <section class="">
     <div class="text-sm/6">
-      <div class="py-2 pt-0 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <UserCircleIcon class="h-4.5 w-4.5 text-gray-600" />
-            Recipient
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900 font-normal text-sm/7">{{ quote.recipient.wholeName }}</div>
-        </dd>
-      </div>
-      <div class="py-2 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <FlagIcon class="h-4.5 w-4.5 text-gray-600" />
-            Payout Country
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ quote.payoutCountry.commonName }}</div>
-        </dd>
-      </div>
-      <div class="py-2 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <TruckIcon class="h-4.5 w-4.5 text-gray-600" />
-            Payout Method
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ quote.payoutMethod.title }}</div>
-        </dd>
-      </div>
-      <div v-for="accountDetailHashmap in quote.recipient.accountDetailHashMap" class="py-2 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <UserCircleIcon v-if="accountDetailHashmap.type === RecipientDataType.ACCOUNT_HOLDER_NAME" class="h-4.5 w-4.5 text-gray-600" />
-            <BuildingLibraryIcon v-if="accountDetailHashmap.type === RecipientDataType.DELIVERY_OPTION" class="h-4.5 w-4.5 text-gray-600" />
-            <DocumentCurrencyDollarIcon v-if="accountDetailHashmap.type === RecipientDataType.ACCOUNT_NUMBER" class="h-4.5 w-4.5 text-gray-600" />
-            {{ accountDetailHashmap.key }}
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ accountDetailHashmap.value }}</div>
-        </dd>
-      </div>
-      <div class="py-2 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <PaperAirplaneIcon class="h-4.5 w-4.5 text-gray-600" />
-            Sending Amount
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ quote.localAmountCurrencyPrefixed }}</div>
-        </dd>
-      </div>
-      <div class="py-2 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <BanknotesIcon class="h-4.5 w-4.5 text-gray-600" />
-            Exchange Rate
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ quote.exchangeRateFormatted }}</div>
-        </dd>
-      </div>
-      <div class="py-2 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <BuildingLibraryIcon class="h-4.5 w-4.5 text-gray-600" />
-            Payout Amount
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ quote.foreignAmountCurrencyPrefixed }}</div>
-        </dd>
-      </div>
-      <div class="py-2 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <PlusIcon class="h-4.5 w-4.5 text-gray-600" />
-            Fees
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ quote.baseFeesFormatted }}</div>
-        </dd>
-      </div>
-      <div class="py-2 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <CalculatorIcon class="h-4.5 w-4.5 text-gray-600" />
-            Sub Total
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ quote.localAmountCurrencyPrefixed }}</div>
-        </dd>
-      </div>
-      <div class="py-2 pb-5 px-5 sm:flex">
-        <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-          <div class="flex justify-start items-center gap-4 text-sm/7">
-            <WalletIcon class="h-4.5 w-4.5 text-gray-600" />
-            Total Due
-          </div>
-        </dt>
-        <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-          <div class="text-gray-900">{{ quote.localAmountCurrencyPrefixed }}</div>
-        </dd>
-      </div>
+      <template v-for="reviewItem in reviewItems">
+        <div class="py-2 px-0 sm:px-5 flex space-x-6 flex-col sm:flex-row">
+          <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none">
+            <div class="flex justify-start items-center gap-4 text-sm/7">
+              <component :is="reviewItem.icon" class="h-4.5 w-4.5 text-gray-600" />
+              {{ reviewItem.label }}
+            </div>
+          </dt>
+          <dd class="ml-8.5 sm:ml-0">
+            <div class="text-gray-900 font-normal text-sm/7">{{ reviewItem.value }}</div>
+          </dd>
+        </div>
+      </template>
     </div>
   </section>
 </template>
