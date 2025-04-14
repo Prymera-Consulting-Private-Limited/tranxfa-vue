ade3cd4791d81280245c28571d251bdaa66f8d5d
Tommy Shelby <dhruv@remitso.com>
2025-04-14 16:58:19 +0530

    feat(Failed): add Lottie animation for enhanced visual feedback

---
+++ src/components/Payment/State/Failed.vue
@@ -0,0 +1,7 @@
+<script setup>
+import {DotLottieVue} from "@lottiefiles/dotlottie-vue";
+</script>
+
+<template>
+  <DotLottieVue class="size-64 mx-auto" autoplay loop src="https://lottie.host/58151b7e-d478-46d3-912c-5dec744fafa7/ZnnrITjGIr.lottie" />
+</template>
