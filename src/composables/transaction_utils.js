67e3b2f85a2e1c60859ac7b75e9ada4d614ee7f7
Tommy Shelby <dhruv@remitso.com>
2025-04-14 17:13:06 +0530

    feat(transaction_utils): add retryPayment function for handling payment retries

--- src/composables/transaction_utils.js
+++ src/composables/transaction_utils.js
@@ -4,12 +4,17 @@ export function useTransactionUtils() {
         return axios.get(`/client/v1/transaction/${id}`);
     }
 
+    const retryPayment = async (id) => {
+        return axios.post(`/client/v1/transaction/payment/${id}`);
+    }
+
     const get = async () => {
         return axios.get(`/client/v1/transactions`);
     }
 
     return {
         get,
+        retryPayment,
         getTransaction,
     }
 }
