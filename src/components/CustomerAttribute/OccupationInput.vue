de59a7c9f3960d884aea4680c82cba7f7db57d25
Tommy Shelby <dhruv@remitso.com>
2025-04-19 17:13:46 +0530

    Other Occupation Error Handled

--- src/components/CustomerAttribute/OccupationInput.vue
+++ src/components/CustomerAttribute/OccupationInput.vue
@@ -1,8 +1,7 @@
 <script setup>
 import CustomerAttribute from "@/models/customer_attribute.js";
 import vSelect from 'vue-select';
-import {h, ref, watch, watchEffect} from "vue";
-import TextInput from "@/components/CustomerAttribute/TextInput.vue";
+import {computed, h, ref, watchEffect} from "vue";
 
 const props = defineProps({
   attr: {
@@ -12,7 +11,11 @@ const props = defineProps({
   occupations: {
     type: Array,
     required: true,
-  }
+  },
+  formErrors: {
+    type: Object,
+    required: false,
+  },
 })
 
 const emit = defineEmits(['customer:attribute:updated']);
@@ -40,6 +43,14 @@ watchEffect(() => {
   });
 })
 
+const otherOccupationError = computed(() => {
+  if (props.formErrors?['employment.other_occupation'] : null) {
+    return props.formErrors['employment.other_occupation'];
+  }
+
+  return null;
+})
+
 </script>
 
 <template>
@@ -65,7 +76,8 @@ watchEffect(() => {
   </v-select>
 
   <template v-if="selectedOccupation?.code === 'OTHER'">
-    <p class="my-3 text-gray-400 text-sm">Please enter your occupation in input below</p>
-    <input :id="attr.attribute" :name="'other_occupation'" :required="attr.isRequired" type="text" v-model="otherOccupation" :class="['block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none']" />
+    <p :class="[otherOccupationError ? 'text-red-700' : 'text-gray-400']" class="my-3 text-sm">Please enter your occupation in input below</p>
+    <input :id="attr.attribute" :name="'other_occupation'" type="text" v-model="otherOccupation" :class="['block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none']" />
+    <p v-if="otherOccupationError?.length > 0" class="mt-2 text-sm text-red-600 dark:text-red-500">{{ otherOccupationError[0] }}</p>
   </template>
 </template>
