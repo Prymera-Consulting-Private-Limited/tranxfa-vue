bef426b8c4c06b85284681ad828e74f4f9597e74
Tommy Shelby <dhruv@remitso.com>
2025-04-26 04:04:53 +0530

    Model Updated

--- src/models/payment_transaction.js
+++ src/models/payment_transaction.js
@@ -2,6 +2,7 @@ import PaymentTransactionState from "@/models/payment_transaction_state.js";
 import PaymentMethod from "@/models/payment_method.js";
 import PaymentProvider from "@/models/payment_provider.js";
 import PaymentAccount from "@/models/payment_account.js";
+import ClientPaymentAccount from "@/models/client_payment_account.js";
 
 class PaymentTransaction {
     /**
@@ -13,6 +14,11 @@ class PaymentTransaction {
      * @type {Object|null}
      */
     paymentAccount = null;
+
+    /**
+     * @type {ClientPaymentAccount|null}
+     */
+    clientPaymentAccount = null;
 
     /**
      * @type {Object|null}
@@ -76,6 +82,9 @@ class PaymentTransaction {
         paymentTransaction.updatedAt = data.updated_at;
         if (data.state) {
             paymentTransaction.state = PaymentTransactionState.getInstance(data.state);
+        }
+        if (data.client_payment_account) {
+            paymentTransaction.clientPaymentAccount = ClientPaymentAccount.getInstance(data.client_payment_account);
         }
 
         return paymentTransaction;
