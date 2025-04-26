7871a2995312bbf098c1bcea8c927e8e4d5b05c4
Tommy Shelby <dhruv@remitso.com>
2025-04-26 17:44:42 +0530

    Color Updated

--- src/components/Payment/ManualPayment.vue
+++ src/components/Payment/ManualPayment.vue
@@ -90,9 +90,9 @@ const status = computed(() => {
           <UseClipboard v-slot="{ copy, copied }" :source="accountAttribute.value">
             <div class="mt-2 flex">
               <div class="-mr-px grid grow grid-cols-1 focus-within:relative">
-                <input type="text" readonly :value="accountAttribute.value" :id="`account-attribute-${index}`" class="col-start-1 row-start-1 block w-full rounded-l-md bg-gray-50 py-2.5 px-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-purple-600 sm:text-sm/6" />
+                <input type="text" readonly :value="accountAttribute.value" :id="`account-attribute-${index}`" class="col-start-1 row-start-1 block w-full rounded-l-md bg-gray-50 py-2.5 px-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6" />
               </div>
-              <button @click="copy()" type="button" class="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-gray-100 px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-purple-600 cursor-pointer">
+              <button @click="copy()" type="button" class="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-gray-100 px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 cursor-pointer">
                 <ClipboardIcon class="-ml-0.5 size-4 text-gray-400" aria-hidden="true" />
               </button>
             </div>
@@ -104,9 +104,9 @@ const status = computed(() => {
           <UseClipboard v-slot="{ copy, copied }" :source="transaction.payment.clientPaymentAccount.paymentReference">
             <div class="mt-2 flex">
               <div class="-mr-px grid grow grid-cols-1 focus-within:relative">
-                <input type="text" readonly :value="transaction.payment.clientPaymentAccount.paymentReference" :id="`payment-reference`" class="col-start-1 row-start-1 block w-full rounded-l-md bg-gray-50 py-2.5 px-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-purple-600 sm:text-sm/6" />
+                <input type="text" readonly :value="transaction.payment.clientPaymentAccount.paymentReference" :id="`payment-reference`" class="col-start-1 row-start-1 block w-full rounded-l-md bg-gray-50 py-2.5 px-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6" />
               </div>
-              <button @click="copy()" type="button" class="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-gray-100 px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-purple-600 cursor-pointer">
+              <button @click="copy()" type="button" class="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-gray-100 px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 cursor-pointer">
                 <ClipboardIcon class="-ml-0.5 size-4 text-gray-400" aria-hidden="true" />
               </button>
             </div>
@@ -114,9 +114,9 @@ const status = computed(() => {
           </UseClipboard>
         </div>
         <div class="mt-6">
-          <button @click="iHaveMadePayment" type="button" class="rounded-md w-full bg-purple-600 px-6 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-purple-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600 cursor-pointer">I've made payment</button>
+          <button @click="iHaveMadePayment" type="button" class="rounded-md w-full bg-blue-600 px-6 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 cursor-pointer">I've made payment</button>
         </div>
-        <div class="my-6 leading-6 text-center text-gray-900 hover:text-purple-700 font-semibold text-sm">
+        <div class="my-6 leading-6 text-center text-gray-900 hover:text-blue-700 font-semibold text-sm">
           <router-link :to="{name: 'viewTransaction', params: {transactionId: transaction.id}}">View Transaction</router-link>
         </div>
       </template>
