592ac951b78e707be353b86e9bcd68f4c54bfe0e
Tommy Shelby <dhruv@remitso.com>
2025-08-18 18:36:46 +0530

    Unselect Event Handled

--- src/components/Recipient/Attribute/DeliveryOptionInput.vue
+++ src/components/Recipient/Attribute/DeliveryOptionInput.vue
@@ -36,6 +36,10 @@ const optionSelected = (option) => {
   emit('recipient:input:updated', option, props.attribute);
 };
 
+const optionRemoved = () => {
+  emit('recipient:input:updated', null, props.attribute);
+};
+
 function withPopper(dropdownList, component, { width }) {
   dropdownList.style.width = width;
   const popper = createPopper(component.$refs.toggle, dropdownList, {
@@ -69,7 +73,7 @@ function withPopper(dropdownList, component, { width }) {
 </script>
 
 <template>
-  <v-select v-on:option:selected="optionSelected" :calculate-position="withPopper" v-model="deliveryOption" :options="attribute.options" :placeholder="`${placeholder}`" key-by="id" label="title">
+  <v-select v-on:option:selected="optionSelected" v-on:option:deselected="optionRemoved" :calculate-position="withPopper" v-model="deliveryOption" :options="attribute.options" :placeholder="`${placeholder}`" key-by="id" label="title">
     <template v-slot:no-options="{ search, searching }">
       <template class="text-sm text-gray-300" v-if="searching">No results found for <em>{{ search }}</em>.</template>
       <em class="text-sm text-gray-400 opacity-50" v-else>Start typing to search ...</em>
