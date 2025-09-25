a1e77acc92f9598f11585d01b27306dd9f2de325
Tommy Shelby <dhruv@remitso.com>
2025-09-25 12:42:53 +0530

    Branding Updated

--- src/views/ForgotPasswordView.vue
+++ src/views/ForgotPasswordView.vue
@@ -36,7 +36,7 @@ async function requestResetPassword() {
           <img src="/images/backgrounds/login.png" alt="Login Background" class="w-full h-90 md:h-full object-cover hidden md:block">
           <!-- Logo and Cross in Mobile View -->
           <div class="absolute top-4 left-4 md:hidden flex items-center justify-between w-full px-4">
-            <a href="javascript:"><img src="/images/logo.png" alt="RemitSo Logo" class="max-w-64 max-h-10 mb-5"></a>
+            <a href="javascript:"><img src="/images/logo.png" alt="Tuhfapay Logo" class="max-w-64 max-h-10 mb-5"></a>
             <a href="javascript:" class="text-gray-400 text-3xl hover:text-gray-500 pr-5">
               <i class="pi pi-times"></i>
             </a>
@@ -53,7 +53,7 @@ async function requestResetPassword() {
           <div class="w-full max-w-xl">
             <!-- Logo at Top Left (Desktop)  -->
             <div class="hidden md:block">
-              <a href="javascript:"><img src="/images/logo.png" alt="RemitSo Logo" class="max-w-64 max-h-10 mb-5"></a>
+              <a href="javascript:"><img src="/images/logo.png" alt="Tuhfapay Logo" class="max-w-64 max-h-10 mb-5"></a>
             </div>
             <!-- Form Header -->
             <h2 class="text-2xl font-bold text-black mb-2">Forgot Password</h2>
