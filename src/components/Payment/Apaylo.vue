6b3a5b1b876c1566793c57b8b76b10847fc49109
Tommy Shelby <dhruv@remitso.com>
2025-10-07 14:48:07 +0530

    Adapter Updated

--- src/components/Payment/Apaylo.vue
+++ src/components/Payment/Apaylo.vue
@@ -41,6 +41,7 @@ onMounted(async () => {
       .listen('PaymentTransactionStateUpdated', (e) => {
         props.transaction.payment.state = PaymentTransactionState.getInstance(e.state);
         props.transaction.payment.sharedReference = e.shared_reference;
+        props.transaction.payment.paymentUrl = e.payment_url;
         if (props.transaction.payment.state.code === PaymentState.PENDING) {
           clearPullInterval();
         } else if (props.transaction.payment.state.code === PaymentState.AUTHORIZED || props.transaction.payment.state.code === PaymentState.CAPTURED) {
@@ -103,7 +104,7 @@ const retryPayment = async () => {
       <p class="text-sm/6 text-gray-600 mb-6 text-left">
         Your transaction is awaiting payment. Please proceed by clicking the button below to securely complete your Interac e-Transfer.
       </p>
-      <a :href="`https://gateway-web.fit.interac.ca/reqPayment/eml/` + transaction.payment.sharedReference" target="_blank" class="block w-full px-4 md:px-6 lg:px-8 bg-green-600 text-white text-center py-3 rounded-md font-medium hover:bg-green-700 transition cursor-pointer text-sm outline-none ring-0 tracking-wider">Pay {{ transaction.payment.totalPaymentAmountCurrencyPrefixed }}</a>
+      <a :href="transaction.payment.paymentUrl" target="_blank" class="block w-full px-4 md:px-6 lg:px-8 bg-green-600 text-white text-center py-3 rounded-md font-medium hover:bg-green-700 transition cursor-pointer text-sm outline-none ring-0 tracking-wider">Pay {{ transaction.payment.totalPaymentAmountCurrencyPrefixed }}</a>
       <p class="text-sm/6 text-gray-600 mt-4 text-left">You will be redirected to the Interac platform to finalize your payment.</p>
     </div>
   </template>
