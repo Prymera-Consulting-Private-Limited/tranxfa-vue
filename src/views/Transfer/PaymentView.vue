32537217017c1e15162e5ad843e1615609c5f5fb
Tommy Shelby <dhruv@remitso.com>
2025-10-10 01:13:21 +0530

    Payment Provider Implemented

--- src/views/Transfer/PaymentView.vue
+++ src/views/Transfer/PaymentView.vue
@@ -11,6 +11,7 @@ import PagaPayment from "@/components/Payment/PagaPayment.vue";
 import Volume from "@/components/Payment/Volume.vue";
 import Monoova from "@/components/Payment/Monoova.vue";
 import Apaylo from "@/components/Payment/Apaylo.vue";
+import Pay360 from "@/components/Payment/Pay360.vue";
 
 const transactionUtils = useTransactionUtils();
 
@@ -95,6 +96,7 @@ watch(canAttemptPayment, async () => {
                     <Monoova v-if="transaction.payment.paymentProvider.code === 'MONOOVA'" v-bind:transaction="transaction"  />
                     <Volume v-on:retryPayment="retryPayment" v-if="transaction.payment.paymentProvider.code === 'VOLUME-PAYMENTS'" v-bind:transaction="transaction"  />
                     <Apaylo v-on:retryPayment="retryPayment" v-if="transaction.payment.paymentProvider.code === 'APAYLO'" v-bind:transaction="transaction"  />
+                    <Pay360 v-on:retryPayment="retryPayment" v-if="transaction.payment.paymentProvider.code === 'PAY360'" v-bind:transaction="transaction"  />
                   </div>
                 </div>
               </div>
