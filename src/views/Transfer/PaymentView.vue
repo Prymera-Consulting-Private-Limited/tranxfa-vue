ff8dd4d59e3b1b8fa3ea864b45c7305d39859cfc
ANSHUMAN <anshuman@remitso.com>
2026-06-29 17:20:52 +0530

    Feat:Added a close (×) button in the top-right corner of all payment modals.

--- src/views/Transfer/PaymentView.vue
+++ src/views/Transfer/PaymentView.vue
@@ -15,6 +15,7 @@ import Pay360 from "@/components/Payment/Pay360.vue";
 import PayCross from "@/components/Payment/PayCross.vue";
 import Fincode from "@/components/Payment/Fincode.vue";
 import CinetPay from "@/components/Payment/CinetPay.vue";
+import ModalCloseButton from "@/components/ModalCloseButton.vue";
 
 const transactionUtils = useTransactionUtils();
 
@@ -70,6 +71,10 @@ watch(canAttemptPayment, async () => {
     await router.push({name: 'viewTransaction', params: {transactionId: props.id}});
   }
 });
+
+function closePaymentModal() {
+  router.push({name: 'viewTransaction', params: {transactionId: props.id}});
+}
 </script>
 
 <template>
@@ -87,7 +92,7 @@ watch(canAttemptPayment, async () => {
     </main>
   </CustomerLayout>
   <TransitionRoot as="template" :show="isLoading === false">
-    <Dialog class="relative z-10">
+    <Dialog class="relative z-10" @close="closePaymentModal">
       <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 bg-gray-500/75 transition-opacity" />
       </TransitionChild>
@@ -96,7 +101,7 @@ watch(canAttemptPayment, async () => {
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
             <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-              <button class="sr-only"></button>
+              <ModalCloseButton @close="closePaymentModal" />
               <div class="py-0 sm:pb-6 px-6">
                 <div class="mt-3 text-center sm:mt-5">
                   <div v-if="transaction" class="text-center">
